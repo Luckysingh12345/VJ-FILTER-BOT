d7bc5f0372188b7914e2a6e955f978167bd7a5d8
MovieBots <154045205+MovieBots@users.noreply.github.com>
2024-06-12 23:18:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -66,8 +66,8 @@ PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b>- ᴀᴠᴀɪʟᴀʙʟᴇ ᴘʟ�
 OWNER_USERNAME = environ.get('OWNER_USERNAME', 'kingvj01') # owner username without @
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/vj_bot_disscussion')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/vj_bots')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/hd_movie_and_webseries')
+CHNL_LNK = environ.get('CHNL_LNK', ' https://t.me/hd_movies_and_webseries_channel')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
 
